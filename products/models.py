66acf9930296b8221b0bf01c44e46182cfc55161
Paul Kane <kanepa1977@gmail.com>
2016-11-21 22:22:32 +0000

    product images

--- products/models.py
+++ products/models.py
@@ -4,10 +4,14 @@ from django.db  import models
 from django.conf import settings
 from paypal.standard.forms import PayPalPaymentsForm
 
+
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='media/images', default='media/images/Aberdeen')
+    # , default='images/Aberdeen.png')
+
 
 
     @property
